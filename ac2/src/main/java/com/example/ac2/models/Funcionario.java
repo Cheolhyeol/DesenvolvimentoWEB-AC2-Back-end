3c0416726f65c158084466a21fa02073664b91e5
Cheolhyeol <omarrapaz@gmail.com>
2025-04-22 21:15:55 -0300

    testes 2

--- ac2/src/main/java/com/example/ac2/models/Funcionario.java
+++ ac2/src/main/java/com/example/ac2/models/Funcionario.java
@@ -1,6 +1,5 @@
 package com.example.ac2.models;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,10 +16,13 @@ public class Funcionario {
 
     private String nome;
 
-    // Vários funcionários podem estar vinculados a um projeto
     @ManyToOne
     @JoinColumn(name = "projeto_id")
     private Projeto projeto;
+
+    @ManyToOne
+    @JoinColumn(name = "setor_id")
+    private Setor setor;
 
     public Funcionario() {}
 
@@ -30,6 +32,10 @@ public class Funcionario {
 
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -46,5 +52,13 @@ public class Funcionario {
 
     public void setProjeto(Projeto projeto) {
         this.projeto = projeto;
+    }
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
     }
 }
